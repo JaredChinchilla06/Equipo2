3f8f0ff56e9412859ab70c351666bbcbb6ab52b8
NABerganzaEspinoza <00374823@uca.edu.sv>
2024-10-14 14:20:41 -0600

    :memo: add file

---
+++ ejc11.cpp
@@ -0,0 +1,28 @@
+/*programa que convierta dólares a euros
+usar bucle do-while para realizar
+conversiones repetidamente*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    float dolar = 0, euro = 0;
+    cout << "Ingrese una cantidad en dolares para convertir a euros\n";
+    do
+    {
+        cout << "ingrese 0 para salir\n";
+        cin >> dolar;
+        if (dolar > 0)
+        {
+            euro = dolar * 0.92;
+            cout << "La cantidad de dolares en euros seria: " << euro << endl
+                 << endl;
+        }
+        else
+        {
+            cout << "Saliendo...";
+        }
+
+    } while (dolar != 0 /*ingresar 0 para salir*/);
+
+    return 0;
+}
